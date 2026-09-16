3b83917ce76575dcba5e6c6e42de37579dfa3054
aplikasiterpadu85-glitch <aplikasiterpadu85@gmail.com>
2026-09-16 18:04:39 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyBoxGrm5UEJnN5D2AZqdVC0MO7JCYiuL2tf_R0jCOdhisBtyNopbp7NoiCP_olsbLj/exec";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzF_dBpgprj07HLb9eUYaA2Fzy021m5Z5CdX4Uv_S3urYEcTUhKee5EbOHtXHGS0SJYCw/exec";
 
 const safeStorage = {
   _memory: {},
